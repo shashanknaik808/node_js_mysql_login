39c8cad648a0dfa87dcdab8edaeedf019fcec25e
Shashank Naik <shashanknaik808@gmail.com>
2023-10-07 14:36:59 +0530

    Variable creation is complete

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,4 +1,12 @@
-exports.register = (req, res) => {
-    console.log(req.body);
-    res.json("Form sumbitted");
-}
+const mysql = require('mysql');
+
+
+const db = mysql.createConnection({
+    host: process.env.DATABASE_HOST,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE
+});
+
+
+
